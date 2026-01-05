22df503e932aaab6d4875da272f455c4992b5caf
Mario Hugo Marcelo Corte <84399997+mariocorte@users.noreply.github.com>
2026-01-05 12:41:36 -0300

    Add discard update to domicilio SQL

--- difieredomicilioe.sql
+++ difieredomicilioe.sql
@@ -9,3 +9,17 @@ WHERE env.codigoseguimientomp IS NULL
       AND env2.pdomicilioelectronicopj <> env.pdomicilioelectronicopj
       AND env2.codigoseguimientomp IS NOT NULL
   );
+
+UPDATE public.enviocedulanotificacionpolicia AS env
+SET descartada = TRUE,
+    fechadescarte = CURRENT_TIMESTAMP,
+    usuariodescarte = 8880
+WHERE env.codigoseguimientomp IS NULL
+  AND COALESCE(env.descartada, false) = false
+  AND EXISTS (
+    SELECT 1
+    FROM public.enviocedulanotificacionpolicia AS env2
+    WHERE env2.pactuacionid = env.pactuacionid
+      AND env2.pdomicilioelectronicopj <> env.pdomicilioelectronicopj
+      AND env2.codigoseguimientomp IS NOT NULL
+  );
